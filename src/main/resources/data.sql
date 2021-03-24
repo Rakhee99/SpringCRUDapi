0694bd0134499c07de74192db12707b22fc8bb10
Rakhee99 <rakheemagic99@gmail.com>
2021-03-24 21:16:11 +0530

    add 106 in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,4 @@ insert into issue values (102,'Improvements or additions to documentation', 'doc
 insert into issue values (103,'This issue or pull request already exists', 'duplicate');
 insert into issue values (104,'New feature or request', 'enhacement');
 insert into issue values (105,'This doesnt seem right', 'invalid');
+insert into issue values (106,'This doesnt seem right', 'invalid');
