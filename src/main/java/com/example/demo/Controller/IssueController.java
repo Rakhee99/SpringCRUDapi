adef37aebc9b2beabaf917b8fbef1969fe8c8934
Rakhee99 <rakheemagic99@gmail.com>
2021-03-24 21:04:32 +0530

    fist cmt Crud vth contrller

---
+++ src/main/java/com/example/demo/Controller/IssueController.java
@@ -0,0 +1,55 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import com.example.demo.dao.IssueRepo;
+import com.example.demo.model.Issue;
+import com.sun.el.stream.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@RestController
+public class IssueController
+{
+	@Autowired
+	IssueRepo repo;
+
+	@DeleteMapping("/issue/{aid}")
+	public String deleteIssue(@PathVariable int aid)
+	{
+		Issue a=repo.getOne(aid);
+		repo.delete(a);
+		return "deleted";
+	}
+	@PutMapping(path="/issue")
+	public Issue saveorUpdate(@RequestBody Issue issue)
+	{
+		repo.save(issue);
+		return issue;
+	}
+	@PostMapping(path="/issue")
+	public Issue addIssue(@RequestBody Issue issue)
+	{
+		repo.save(issue);
+		return issue;
+	}
+	@GetMapping(path="/issue")
+	public List<Issue> getIssues()
+	{
+		return repo.findAll();
+	}
+	@RequestMapping("/issue/{aid}")
+	public java.util.Optional<Issue> getIssue(@PathVariable("aid") int aid)
+	{
+		return repo.findById(aid);
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into issue values (101,'somethng isnt working', 'bug');
+insert into issue values (102,'Improvements or additions to documentation', 'documention');
+insert into issue values (103,'This issue or pull request already exists', 'duplicate');
+insert into issue values (104,'New feature or request', 'enhacement');
+insert into issue values (105,'This doesnt seem right', 'invalid');
